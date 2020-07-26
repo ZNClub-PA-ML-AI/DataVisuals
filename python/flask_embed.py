006658b4e3b16103a9ab65ae71b0b021c2a7d4a9
ZNevzz <dsouza.nevil45@gmail.com>
2020-07-26 12:58:11 +0530

    BokehApp created, slider not working

--- python/flask_embed.py
+++ python/flask_embed.py
@@ -14,31 +14,62 @@ from bokeh.themes import Theme
 
 app = Flask(__name__)
 
+class BokehApp(object):
+    """
+    Start Bokeh App
+    """
+    
+    def __init__(self, file_name=r"../data/nifty50.csv",):
+        self.data_sets = self.create_data_sets(file_name)
 
-def bkapp(doc):
-    df = pd.read_csv(r"../data/nifty50.csv").groupby(['date']).max().reset_index()
-    df['time'] = pd.to_datetime(df['date'], infer_datetime_format=True)
-    df['temperature'] = df['close']
-    source = ColumnDataSource(data=df)
-
-    plot = figure(x_axis_type='datetime', y_range=(0, 25), y_axis_label='Temperature (Celsius)',
-                  title="Sea Surface Temperature at 43.18, -70.43")
-    plot.line('time', 'temperature', source=source)
-
-    def callback(attr, old, new):
-        if new == 0:
-            data = df
-        else:
-            data = df.rolling('{0}D'.format(new)).mean()
-        source.data = ColumnDataSource.from_df(data)
-
-    slider = Slider(start=0, end=30, value=0, step=1, title="Smoothing by N Days")
-    slider.on_change('value', callback)
-
-    doc.add_root(column(slider, plot))
-
-    doc.theme = Theme(filename="theme.yaml")
-
+    def create_data_sets(self, file_name):
+        """
+        
+        """
+        main = pd.read_csv(file_name)
+        max_by_date = main.groupby(['date']).max().reset_index()
+        
+        max_by_date['time'] = pd.to_datetime(max_by_date['date'], infer_datetime_format=True)
+        max_by_date['temperature'] = max_by_date['close']
+        
+        # TODO: Remove ColumnDataSource and let bokeh convert internally
+        # Looks like callback requires a ColumnDataSource object
+        
+        return {
+                'MAIN': ColumnDataSource(data=main), 
+                'MAX_BY_DATE': ColumnDataSource(data=max_by_date)
+                }
+    
+    def add_time_series(self, data_name='MAX_BY_DATE', x_name='time', y_name='temperature'):
+        plot = figure(plot_width=888, plot_height=444, x_axis_type='datetime')
+        plot.line(x_name, y_name, source=self.data_sets[data_name])
+        return plot
+    
+    def add_slider(self, data_name='MAX_BY_DATE', x_name='time', y_name='temperature'):
+        
+        def slider_callback(attribute, old_value, new_value):
+            print('Callback', attribute, old_value, new_value)
+            df = pd.DataFrame(self.data_sets[data_name].data)
+            print('DataFrame', df)
+            df = df[df[y_name] < new_value]
+#            self.data_sets[data_name] = ColumnDataSource(df)
+            
+        
+        df = self.data_sets[data_name].data
+        min_value, max_value = df[y_name].min(), df[y_name].max()
+        
+        slider = Slider(start=min_value, end=max_value, value=0, step=1, title="Slider")
+        slider.on_change('value', slider_callback)
+        
+        return slider
+        
+    def add_figures(self, document):
+        
+        time_series = self.add_time_series()
+        controls = self.add_slider()
+        
+        document.add_root(column(controls, time_series))
+        document.theme = Theme(filename="theme.yaml")    
 
 @app.route('/', methods=['GET'])
 def bkapp_page():
@@ -49,7 +80,11 @@ def bkapp_page():
 def bk_worker():
     # Can't pass num_procs > 1 in this configuration. If you need to run multiple
     # processes, see e.g. flask_gunicorn_embed.py
-    server = Server({'/bkapp': bkapp}, io_loop=IOLoop(), allow_websocket_origin=["localhost:8000"])
+    bokek_app = BokehApp()
+    server = Server({'/bkapp': bokek_app.add_figures}, 
+                    io_loop=IOLoop(), 
+                    allow_websocket_origin=["localhost:8000"]
+                    )
     server.start()
     server.io_loop.start()
 
